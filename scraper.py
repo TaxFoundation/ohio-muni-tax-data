2dfc56e9197f1791bffd72ef92fc250de7eaf2cd
tvanantwerp <tom@tomvanantwerp.com>
2017-03-28 13:47:04 -0400

    Get Addresses

--- scraper.py
+++ scraper.py
@@ -3,4 +3,10 @@ import urllib.request
 
 # soup = BeautifulSoup()
 tax_data = urllib.request.urlopen(urllib.request.Request('https://www.columbus.gov/tax/PrintAllMunis.aspx'))
-print(tax_data.read().decode('utf-8'))
+
+
+soup = BeautifulSoup(tax_data.read().decode('utf-8'), 'html.parser')
+
+addresses = soup.find_all('td', {'class': 'mucip_cont_address'})
+for address in addresses:
+  print(address.get_text())
